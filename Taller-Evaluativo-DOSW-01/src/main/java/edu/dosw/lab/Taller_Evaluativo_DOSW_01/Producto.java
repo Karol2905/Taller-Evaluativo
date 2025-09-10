5ed9d4a0d6f088e22b8b58180d8975d626ea24fd
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2025-09-09 22:36:45 -0500

    Agregar producto

--- Taller-Evaluativo-DOSW-01/src/main/java/edu/dosw/lab/Taller_Evaluativo_DOSW_01/Producto.java
+++ Taller-Evaluativo-DOSW-01/src/main/java/edu/dosw/lab/Taller_Evaluativo_DOSW_01/Producto.java
@@ -1,4 +1,17 @@
 package edu.dosw.lab.Taller_Evaluativo_DOSW_01;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Producto {
+    private int stock;
+    private int precio;
+    private String nombre;
+    private String categoria;
 }
--- Taller-Evaluativo-DOSW-01/src/main/java/edu/dosw/lab/Taller_Evaluativo_DOSW_01/Sistema.java
+++ Taller-Evaluativo-DOSW-01/src/main/java/edu/dosw/lab/Taller_Evaluativo_DOSW_01/Sistema.java
@@ -1,4 +1,19 @@
 package edu.dosw.lab.Taller_Evaluativo_DOSW_01;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@NoArgsConstructor
 public class Sistema {
+    private ArrayList<Producto> productos;
+    private ArrayList<Observador> observadores;
+
+    public void agregarProducto(String nombreProducto,String categoriaProducto, int cantidadStock, int precio){
+        productos.add(new Producto(cantidadStock,precio,nombreProducto,categoriaProducto));
+    }
+
 }
